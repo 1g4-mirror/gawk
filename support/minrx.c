55027abf85c66c0c3946c887268dacc13122e1ab
Arnold D. Robbins <arnold@skeeve.com>
2025-08-24 11:45:11 +0300

    Revert use of iswdigit.

--- support/minrx.c
+++ support/minrx.c
@@ -766,7 +766,7 @@ struct Compile {
 		auto satmul = [](NInt x, NInt y) -> NInt {
 			return (x == 0 || y == 0) ? 0 : ((x * y / x == y) ? x * y : -1);
 		};
-		if (! iswdigit(wc))
+		if (wc < L'0' || wc > L'9')
 			return false;
 		NInt v = 0;
 		do {
@@ -774,12 +774,12 @@ struct Compile {
 			if (v == (NInt) -1 || (v += wc - L'0') < (NInt) wc - L'0') {
 				do
 					wc = wconv.nextchr();
-				while (iswdigit(wc));
+				while (wc >= L'0' && wc <= L'9');
 				n = -1;
 				return true;
 			}
 			wc = wconv.nextchr();
-		} while (iswdigit(wc));
+		} while (wc >= L'0' && wc <= L'9');
 		n = v;
 		return true;
 	}
